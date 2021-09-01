0cc052cefa6b534eb4cd87adfb35b4bb56f47326
ouyangxianghui <ouyangxianghui@guoquan.com>
2021-09-01 16:41:47 +0800

    v1.0

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,15 +1,113 @@
 <template>
   <div>
-    <clock></clock>
+    <div>
+      <el-upload
+        class="upload-demo"
+        :http-request="(evet) => getFile(evet)"
+        :show-file-list="false"
+        ref="upload"
+        action="string"
+      >
+        <el-button slot="trigger" size="small" type="primary"
+          >选取文件上传</el-button
+        >
+      </el-upload>
+    </div>
+    <el-divider></el-divider>
+    <div>
+      <el-table :data="tableData" border style="width: 100%">
+        <el-table-column
+          v-for="item in tableKeys"
+          :key="item"
+          :prop="item"
+          :label="item"
+        >
+        </el-table-column>
+      </el-table>
+      <el-button
+        @click="reSearch"
+        size="mini"
+        type="primary"
+        style="margin-top: 5px"
+        >重新查询</el-button
+      >
+    </div>
+    <el-divider></el-divider>
+    <div>
+      <el-form ref="form" size="mini" :model="form" label-width="60px">
+        <el-form-item label="Pt" prop="Pt">
+          <el-input v-model="form.Pt"></el-input>
+        </el-form-item>
+        <el-form-item label="Pr" prop="Pr">
+          <el-input
+            type="textarea"
+            v-model="form.Pr"
+            placeholder="请用换行隔开"
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="Pjthread" prop="Pjthread">
+          <el-input v-model="form.Pjthread"></el-input>
+        </el-form-item>
+        <el-form-item label="Pdura" prop="Pdura">
+          <el-input v-model="form.Pdura"></el-input>
+        </el-form-item>
+      </el-form>
+      <el-button size="mini" @click="submit" type="primary">点击发送</el-button>
+    </div>
   </div>
 </template>
 
 <script>
-import clock from "@/components/clock.vue";
+import axios from "axios";
+
 export default {
-  components: {
-    clock,
+  components: {},
+  data() {
+    return {
+      tableData: [
+        {
+          iiie: 11,
+        },
+      ],
+      text: 22222,
+      form: { Pt: "str", Pr: "", Pjthread: "str", Pdura: "str" },
+    };
   },
+  computed: {
+    tableKeys() {
+      return Object.keys(this.tableData[0]);
+    },
+  },
+  methods: {
+    getFile(e) {
+      const para = new FormData();
+      para.append("file", e.file);
+      console.log("1", 1);
+      axios.post("/jmx/FileUpLoad", para).then((response) => {
+        this.text = response.data;
+        this.$message.info(response.data || response);
+        console.log(response);
+      });
+    },
+    reSearch() {
+      axios.post("/jmx/jmxList").then((response) => {
+        this.tableData = response.data;
+        this.$message.info(response.data || response);
+        console.log(response);
+      });
+    },
+    submit() {
+      let para = Object.assign({}, this.form);
+      para.Pr = para.Pr.split(/\s+/);
+      console.log("para", para);
+      axios.post("/jmx/jmxList", para).then((response) => {
+        this.text = response;
+        this.$message.info(response.data || response);
+        console.log(response);
+      });
+    },
+  },
+  mounted() {},
 };
 </script>
 
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,44 +1,10 @@
-const routesArr = [
-  {
-    path: "/2021-05-12",
-    name: "wuwuw",
-    meta: {
-      date: "2021-05-12",
-      desc: "CSS3 :nth-child() 选择器",
-    },
-    component: () => import("@/pages/2021/2021-05-12.vue"),
-  },
-  {
-    path: "/2021-06-25",
-    name: "wuwuw",
-    meta: {
-      date: "2021-06-25",
-      desc: "Number计算问题",
-    },
-    component: () => import("@/pages/2021/2021-06-25.vue"),
-  },
-  {
-    path: "/2021-06-26",
-    name: "wuwuw",
-    meta: {
-      date: "2021-06-26",
-      desc: "Number计算问题解决方案",
-    },
-    component: () => import("@/pages/2021/2021-06-26.vue"),
-  },
-];
-
 const routes = [
   {
-    path: "",
-    component: () => import("@/components/layout.vue"),
-    children: routesArr.concat({
       path: "*",
       meta: {
         desc: "home page",
       },
       component: () => import("@/pages/home/index.vue"),
-    }),
   },
 ];
 
