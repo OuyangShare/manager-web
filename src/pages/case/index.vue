f39db77e55e0a55e156f2fa0513660997b690eb4
ouyangxianghui <ouyangxianghui@guoquan.com>
2022-01-21 16:40:06 +0800

    页面完成

--- src/pages/case/index.vue
+++ src/pages/case/index.vue
@@ -14,7 +14,17 @@
       :picker-options="pickerOptionsDate"
     >
     </el-date-picker>
-    <div v-show="showCharts" class="charts"></div>
+    <el-tabs v-model="activeTab" type="card" @tab-click="handleClick">
+      <el-tab-pane label="taExes" name="taExes">
+        <div v-show="showCharts" class="charts charts_taExes"></div>
+      </el-tab-pane>
+      <el-tab-pane label="source" name="source">
+        <div v-show="showChartsSource" class="charts charts_source"></div>
+      </el-tab-pane>
+      <el-tab-pane label="user" name="user">
+        <div v-show="showChartsUser" class="charts charts_user"></div>
+      </el-tab-pane>
+    </el-tabs>
   </div>
 </template>
 
@@ -26,9 +36,29 @@ export default {
   data() {
     return {
       dayjs: dayjs,
+      activeTab: "taExes",
+      colorArr: [
+        "blue",
+        "red",
+        "yellow",
+        "green",
+        "#33B5E5",
+        "#0099CC",
+        "#AA66CC",
+        "#9933CC",
+        "#99CC00",
+        "#669900",
+        "#FFBB33",
+        "#FF8800",
+        "#FF4444",
+        "#CC0000",
+      ],
+      initData: {},
       showCharts: true,
+      showChartsSource: false,
+      showChartsUser: false,
       settleStartDate: dayjs(
-        new Date().setHours(23, 59, 59) - 3600 * 1000 * 24 * 7
+        new Date().setHours(0, 0, 0) - 3600 * 1000 * 24 * 6
       ).format("YYYY-MM-DD HH:mm:ss"),
       settleEndDate: dayjs(new Date().setHours(23, 59, 59)).format(
         "YYYY-MM-DD HH:mm:ss"
@@ -78,7 +108,7 @@ export default {
       set: function (val) {
         if (val) {
           this.settleStartDate = this.dayjs(val[0]).format(
-            "YYYY-MM-DD HH:mm:ss"
+            "YYYY-MM-DD 00:00:00"
           );
           this.settleEndDate = this.dayjs(val[1]).format("YYYY-MM-DD HH:mm:ss");
         } else {
@@ -86,6 +116,12 @@ export default {
           this.settleEndDate = "";
         }
       },
+    },
+    diffDate() {
+      const diffTimeStamp =
+        new Date(this.settleEndDate).getTime() -
+        new Date(this.settleStartDate).getTime();
+      return parseInt(diffTimeStamp / 86400000 || 7);
     },
   },
   methods: {
@@ -98,27 +134,49 @@ export default {
           `/api/taexe/caseExeRep?startAt=${this.settleStartDate}&endAt=${this.settleEndDate}`
         )
         .then((res) => {
-          this.showCharts = true;
-          console.log(res);
-          this.increaseBuild(res.data.data);
+          this.initData = res.data.data;
+          this.increaseTaexesBuild(this.initData.taExes);
+          this.increaseSoureBuild(this.initData.source);
+          this.increaseUserBuild(this.initData.user);
         })
         .catch((err) => {
           console.log(err);
           this.showCharts = false;
+          this.showChartsSource = false;
+          this.showChartsUser = false;
           this.$message.error("数据请求失败！");
         });
     },
-    increaseBuild(obj) {
-      console.log(obj);
-      const namesArr = Array.from(obj, (x) =>
-        this.dayjs(x.exeat).format("YYYY.MM.DD")
-      );
-      console.log(namesArr);
-      const valueArr = obj.map((x) => {
-        return {
-          value: x.count,
-          id: x.id,
-        };
+    increaseTaexesBuild(obj) {
+      const namesArr = [];
+      const valueArr = [];
+      for (let i = 0; i <= this.diffDate; i++) {
+        namesArr.push(
+          this.dayjs(
+            new Date(this.settleStartDate).setHours(24 * i + 1)
+          ).format("YYYY.MM.DD")
+        );
+      }
+      namesArr.forEach((e) => {
+        const valItem = obj.find((x) => {
+          return (
+            this.dayjs(e).format("YYYY.MM.DD") ===
+            this.dayjs(x.exeat).format("YYYY.MM.DD")
+          );
+        });
+        if (valItem) {
+          valueArr.push({
+            value: valItem.count,
+            id: valItem.id,
+            date: valItem.exeat,
+          });
+        } else {
+          valueArr.push({
+            value: 0,
+            id: null,
+            date: null,
+          });
+        }
       });
       const seriesArr = [
         {
@@ -136,7 +194,7 @@ export default {
         },
       ];
       const myChart = echarts.init(
-        document.getElementsByClassName("charts")[0]
+        document.getElementsByClassName("charts_taExes")[0]
       );
       myChart.setOption({
         title: {
@@ -148,6 +206,7 @@ export default {
         tooltip: {
           // show:true,
           // trigger: "axis",
+          triggerOn: "click",
           enterable: true,
           appendToBody: true,
           formatter: function (params) {
@@ -188,6 +247,207 @@ export default {
         },
         series: seriesArr,
       });
+      this.showCharts = true;
+    },
+    increaseSoureBuild(obj) {
+      const namesArr = [];
+      for (let i = 0; i <= this.diffDate; i++) {
+        namesArr.push(
+          this.dayjs(
+            new Date(this.settleStartDate).setHours(24 * i + 1)
+          ).format("YYYY.MM.DD")
+        );
+      }
+      const sourceArr = Object.keys(obj);
+      const seriesArr = [];
+      sourceArr.forEach((s, idx) => {
+        let valueArr = [];
+        namesArr.forEach((e) => {
+          const valItem = obj[s].find((x) => {
+            return (
+              this.dayjs(e).format("YYYY.MM.DD") ===
+              this.dayjs(x.executedAt).format("YYYY.MM.DD")
+            );
+          });
+          if (valItem) {
+            valueArr.push({
+              value: valItem.total,
+              id: valItem.id,
+              date: valItem.executedAt,
+            });
+          } else {
+            valueArr.push({
+              value: 0,
+              id: null,
+              date: null,
+            });
+          }
+        });
+        seriesArr.push({
+          name: s,
+          color: this.colorArr[idx],
+          type: "line",
+          smooth: true,
+          data: valueArr,
+          stack: "s",
+          // areaStyle: {
+          //   opacity: 0.8,
+          //   color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+          //     {
+          //       offset: 0,
+          //       color: this.colorArr[idx],
+          //     },
+          //     {
+          //       offset: 1,
+          //       color: this.colorArr[idx],
+          //     },
+          //   ]),
+          // },
+          label: {
+            show: true,
+            position: "top",
+            valueAnimation: true,
+          },
+        });
+      });
+      const myChart = echarts.init(
+        document.getElementsByClassName("charts_source")[0]
+      );
+      myChart.setOption({
+        title: {
+          text: "执行分布图",
+        },
+        legend: {
+          data: sourceArr,
+        },
+        tooltip: {
+          // show:true,
+          // trigger: "axis",
+          enterable: true,
+          appendToBody: true,
+          // formatter: function (params) {},
+        },
+        xAxis: {
+          data: namesArr,
+          type: "category",
+          axisLabel: {
+            interval: 0,
+            rotate: 70,
+          },
+        },
+        yAxis: {
+          type: "value",
+          name: "数量",
+          min: 0,
+          position: "left",
+          axisLabel: {
+            formatter: "{value}",
+          },
+        },
+        series: seriesArr,
+      });
+      this.showChartsSource = true;
+    },
+    increaseUserBuild(obj) {
+      const namesArr = [];
+      for (let i = 0; i <= this.diffDate; i++) {
+        namesArr.push(
+          this.dayjs(
+            new Date(this.settleStartDate).setHours(24 * i + 1)
+          ).format("YYYY.MM.DD")
+        );
+      }
+      const userArr = Object.keys(obj);
+      const seriesArr = [];
+      userArr.forEach((s, idx) => {
+        let valueArr = [];
+        namesArr.forEach((e) => {
+          const valItem = obj[s].find((x) => {
+            return (
+              this.dayjs(e).format("YYYY.MM.DD") ===
+              this.dayjs(x.executedat).format("YYYY.MM.DD")
+            );
+          });
+          if (valItem) {
+            valueArr.push({
+              value: valItem.countcase,
+              id: valItem.id,
+              date: valItem.executedat,
+            });
+          } else {
+            valueArr.push({
+              value: 0,
+              id: null,
+              date: null,
+            });
+          }
+        });
+        seriesArr.push({
+          name: s,
+          color: this.colorArr[idx],
+          type: "line",
+          smooth: true,
+          data: valueArr,
+          stack: "s",
+          label: {
+            show: true,
+            position: "top",
+            valueAnimation: true,
+          },
+        });
+      });
+      const myChart = echarts.init(
+        document.getElementsByClassName("charts_user")[0]
+      );
+      myChart.setOption({
+        title: {
+          text: "执行分布图",
+        },
+        legend: {
+          data: userArr,
+        },
+        tooltip: {
+          // show:true,
+          // trigger: "axis",
+          enterable: true,
+          appendToBody: true,
+          // formatter: function (params) {},
+        },
+        xAxis: {
+          data: namesArr,
+          type: "category",
+          axisLabel: {
+            interval: 0,
+            rotate: 70,
+          },
+        },
+        yAxis: {
+          type: "value",
+          name: "数量",
+          min: 0,
+          position: "left",
+          axisLabel: {
+            formatter: "{value}",
+          },
+        },
+        series: seriesArr,
+      });
+      this.showChartsUser = true;
+    },
+    handleClick(val) {
+      switch (val) {
+        case "taExes":
+          // this.showCharts = true;
+          break;
+        case "source":
+          // this.increaseSoureBuild(this.initData.source);
+          // this.showChartsSource = true;
+          break;
+        case "user":
+          // this.increaseUserBuild(this.initData.user);
+          // this.showChartsUser = true;
+          break;
+      }
     },
   },
   mounted() {
@@ -200,12 +460,22 @@ export default {
 // p:nth-child(2)
 //     background: #eeeeee
 .charts
-    display: inline-block
-    width: 90vw
-    height: 300px
-    margin-top: 30px
+  display: inline-block
+  width: 90vw
+  height: 300px
+  margin-top: 30px
+// .charts_source
+  // display: inline-block
+  // width: 45vw
+  // height: 300px
+  // margin-top: 30px
+// .charts_user
+  // display: inline-block
+  // width: 45vw
+  // height: 300px
+  // margin-top: 30px
 .mid
-    display: inline-block
-    width: 45vw
-    height: 300px
+  display: inline-block
+  width: 45vw
+  height: 300px
 </style>
